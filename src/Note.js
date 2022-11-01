bd9f5a4d9f36f7eb1742196ece714b848c56eb84
Dmitry Votchitsev <dmitry.votchitsev@gmail.com>
2022-11-01 20:43:50 +0200

    add delete btns

--- src/Note.js
+++ src/Note.js
@@ -2,7 +2,7 @@ function Note({ note }) {
   return (
     <div className="note">
       <div className="note-content">{ note.content }</div>
-      <div className="note-delete-btn"></div>
+      <div className="note-delete-btn">{ 'x' }</div>
     </div>
   )
 }
